04ec7ebc28bc0e780251743fd1aa8728646fa38d
Lamina Samuel <samuellamina2@gmail.com>
2021-10-21 14:04:47 +0200

    commit

---
+++ src/pages/profile/Profile.jsx
@@ -0,0 +1,42 @@
+import "./profile.css";
+import Topbar from "../../components/topbar/Topbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Rightbar from "../../components/rightbar/Rightbar";
+import Feed from "../../components/feed/Feed";
+
+const Profile = () => {
+  return (
+    <>
+      <Topbar />
+      <div className="profile">
+        <Sidebar />
+        <div className="profileRight">
+          <div className="profileContainerTop">
+            <div className="profileCover">
+              <img
+                className="profileCoverImg"
+                src="assets/post/3.jpeg"
+                alt=""
+              />
+              <img
+                className="profileUserImg"
+                src="assets/person/7.jpeg"
+                alt=""
+              />
+            </div>
+            <div className="profileInfo">
+              <h4 classname="profileInfoName">Safak</h4>
+              <span classname="profileInfoDesc">HELOO MEI AIWHREJN </span>
+            </div>
+          </div>
+          <div className="profileRightBottom">
+            <Feed />
+            <Rightbar profile />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Profile;
